b70ba8b1c3b475d1a5081e6af7b8daf1bab0a44e
adi forkasiewicz <adi.forkasiewicz@ingrid.com>
2026-05-21 11:53:30 +0200

    chore: adjust fg0 to a soft neutral gray instead of blue-tinted

--- lua/token/palette.lua
+++ lua/token/palette.lua
@@ -136,7 +136,7 @@ local function palette(background)
     bg4 = '#2f2f2d',
     bg5 = '#383835',
     -- Foreground ramp
-    fg0 = '#a9b8c4',
+    fg0 = '#b8b8b8',
     fg1 = '#999999',
     fg2 = '#777777',
     fg3 = '#555555',
